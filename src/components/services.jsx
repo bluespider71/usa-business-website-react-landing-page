5331f9994bba84130384d9c58f92f3b72f3bdd5e
bluespider71 <jowilliam1028@gmail.com>
2022-11-13 15:01:35 -0500

    first commit

---
+++ src/components/services.jsx
@@ -0,0 +1,29 @@
+export const Services = (props) => {
+  return (
+    <div id='service' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Our services</h2>
+          <p>
+            Our team of professionals will give you a personalized treatment from start to finish in all the procedures and contracted services.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+              <div key={`${d.name}-${i}`} className='col-md-4'>
+                {' '}
+                {/* <i className={d.icon}></i> */}
+                <img src={d.img} className="img-responsive service-img" alt={d.name} />
+                <div className='service-desc'>
+                  <h3>{d.name}</h3>
+                  <p>{d.text}</p>
+                </div>
+              </div>
+            ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
